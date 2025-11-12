646aa4ef2c77e183f15b5eaae2921b37dea3f005
dmitr <dmitrhut@mail.ru>
2025-11-12 14:18:13 +0300

    Реализовано получение всех девайсов с фильтрацией по типам и брендам, а также сделан вывод товаров с лимитом по страницам и лимиту товаров на странице

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000 //Берем порт
 
 const app = express() // создаем экспресс-приложение
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static'))) // Обслуживание статических файлов (HTML, Css, JS, изображения)
 app.use(fileUpload({}))
 app.use('/api', router)
 app.use(errorHandler)
