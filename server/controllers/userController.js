c932f7f65b6a514f8da31b4ace6b6cf646634356
dmitr <dmitrhut@mail.ru>
2025-11-13 14:16:00 +0300

    Доработал эндпоинты для пользователя: registration, user и auth. На данный момент все существующие контроллеры доделаны. Надо также сделать корину и рейтинг

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,52 @@
 const ApiError = require('../error/apiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
+
+const generateJwt = (id, email, role) => {
+	return jwt.sign(
+		{id, email, role},
+		process.env.SECRET_KEY,
+		{expiresIn: '24h'}
+	)
+}
+
 class UserController {
-	async registration(req, res) {
-		res.status(200).json({ message: 'Эндпоинт регистрации' })
+	async registration(req, res, next) {
+		const {email, password, role} = req.body
+
+		if (!email || !password) {
+			return next(ApiError.badRequest("Некорректные e-mail или пароль"))
+		}
+
+		const candidate = await User.findOne({where: {email}})
+		if (candidate) {
+			return next(ApiError.badRequest("Пользователь с таким E-mail уже существует"))
+		}
+		const hashPassword = await bcrypt.hash(password, 5)
+		const user = await User.create({email, role, password: hashPassword})
+		const basket = await Basket.create({userId: user.id})
+		const token = generateJwt(user.id, user.email, user.role)
+		return res.json({token})
 	}
 
-	async login(req, res) {
-		res.status(200).json({ message: 'Эндпоинт авторизации' })
+	async login(req, res, next) {
+		const {email, password} = req.body
+		const user = await User.findOne({where: {email}})
+		if (!user) {
+			return next(ApiError.badRequest("Пользователь не найден"))
+		}
+		let comparePassword = bcrypt.compareSync(password, user.password)
+		if (!comparePassword) {
+			return next(ApiError.badRequest('Указан неверный пароль'))
+		}
+		const token = generateJwt(user.id, user.email, user.role)
+		return res.json({token})
 	}
 
 	async check(req, res, next) {
-		const {id} = req.query // получаем id из строки запроса
-		if (!id) {
-			return next(ApiError.badRequest('Не задан id'))
-		}
-
-		res.status(200).json({ message: `Эндпоинт проверки получил id - ${id}` })
+		const token = generateJwt(req.user.id, req.user.email, req.user.role)
+		res.json({token})
 	}
 }
 
